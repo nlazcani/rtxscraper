6a1c57c58f31fee0782dc4f2d32ba7f4cd272392
Your Name <you@example.com>
2021-08-18 17:48:27 -0400

    start

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM lsiobase/ubuntu:bionic
+
+RUN apt-get update 
+RUN apt-get install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils python3 python3-pip python3-setuptools wget unzip
+#download and install chrome
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
+
+ADD . .
+# install chromedriver
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+
+# set display port to avoid crash
+ENV DISPLAY=:99
+RUN python3 -m pip install -r requirements.txt
+RUN python3 -m pip install webdriver-manager 
+EXPOSE 5001
+CMD ["python3", "checkrtx.py"]
---
+++ checkrtx.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+import telegram
+import os
+import logging
+import sys
+from webdriver_manager.chrome import ChromeDriverManager
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+original = 'Coming Soon'
+
+chrome_options=webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("window-size=1400,2100") 
+chrome_options.add_argument('--disable-gpu')
+
+caps = DesiredCapabilities().CHROME
+caps["pageLoadStrategy"] = "eager"  #  interactive
+
+url = os.getenv('URL')
+txt_clase = os.getenv('TXT_CLASE')
+link = os.getenv('LINK')
+custom_message = os.getenv('MESSAGE')
+sleep_sec = os.getenv('TIME')
+chatid = os.getenv('CHAT')
+
+try:
+    driver = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=caps, chrome_options=chrome_options)
+    driver.get(url)    
+    notice=driver.find_element_by_class_name(txt_clase).text
+    driver.close()
+    if notice != 'Add to Cart':
+        original = notice
+    while True:
+        driver = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=caps, chrome_options=chrome_options)
+        driver.get(url)
+        notice=driver.find_element_by_class_name(txt_clase).text        
+        driver.close()
+        root.debug(f"text getted: {str(notice)}")
+        if notice != original:
+            original = notice
+            bot=telegram.Bot('1906920746:AAEObw_eBqg1PkIBOSiwkIMTkzCWOtgNRUk')
+            bot.send_message(chat_id=chatid, text = f'{notice} {custom_message} {link}')
+            root.info('chat')
+        else:
+            root.info('nothing change')
+        time.sleep(int(sleep_sec))
+finally:
+    driver.close()
