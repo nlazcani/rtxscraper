4941a115a8cd483fe8024f94c18ba0217e131beb
Your Name <you@example.com>
2021-08-19 00:15:01 -0400

    multiple

--- checkrtx.py
+++ checkrtx.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import telegram
 import os
 import logging
 import sys
+import json
 from webdriver_manager.chrome import ChromeDriverManager
 
 root = logging.getLogger()
@@ -13,48 +13,71 @@ root.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 root.addHandler(handler)
 
-original = 'Coming Soon'
+original = []
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("window-size=1400,2100") 
-chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument("window-size=1400,2100")
+chrome_options.add_argument("--disable-gpu")
 
 caps = DesiredCapabilities().CHROME
-caps["pageLoadStrategy"] = "eager"  #  interactive
+caps["pageLoadStrategy"] = "eager"  # interactive
 
-url = os.getenv('URL')
-txt_clase = os.getenv('TXT_CLASE')
-link = os.getenv('LINK')
-custom_message = os.getenv('MESSAGE')
-sleep_sec = os.getenv('TIME')
-chatid = os.getenv('CHAT')
+link = os.getenv("LINK")
+custom_message = os.getenv("MESSAGE")
+sleep_sec = os.getenv("TIME")
+chatid = os.getenv("CHAT")
+DATA_FOLDER = "data"
+urls = []
+notice = []
+with open(os.path.join(DATA_FOLDER, "urls.json"), "r") as file:
+    for line in file:
+        urls.append(json.loads(line))
 
 try:
-    driver = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=caps, chrome_options=chrome_options)
-    driver.get(url)    
-    notice=driver.find_element_by_class_name(txt_clase).text
+    driver = webdriver.Chrome(
+        ChromeDriverManager().install(),
+        desired_capabilities=caps,
+        chrome_options=chrome_options,
+    )
+    for i, url in enumerate(urls):
+        driver.get(url["url"])
+        notice.append("")
+        try:
+            notice[i] = driver.find_element_by_class_name(url["class"]).text
+        except Exception:
+            pass
+        original.append(notice[i])
     driver.close()
-    if notice != 'Add to Cart':
-        original = notice
     while True:
-        driver = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=caps, chrome_options=chrome_options)
-        driver.get(url)
-        notice=driver.find_element_by_class_name(txt_clase).text        
-        driver.close()
-        root.debug(f"text getted: {str(notice)}")
-        if notice != original:
-            original = notice
-            bot=telegram.Bot('1906920746:AAEObw_eBqg1PkIBOSiwkIMTkzCWOtgNRUk')
-            bot.send_message(chat_id=chatid, text = f'{notice} {custom_message} {link}')
-            root.info('chat')
-        else:
-            root.info('nothing change')
-        time.sleep(int(sleep_sec))
+        for i, url in enumerate(urls):
+            driver = webdriver.Chrome(
+                ChromeDriverManager().install(),
+                desired_capabilities=caps,
+                chrome_options=chrome_options,
+            )
+            driver.get(url["url"])
+            try:
+                notice[i] = driver.find_element_by_class_name(url["class"]).text
+            except Exception:
+                notice[i] = ""
+            driver.close()
+            root.debug(f"text getted: {str(notice[i])}")
+            if notice[i] != original[i]:
+                original[i] = notice[i]
+                bot = telegram.Bot("1906920746:AAEObw_eBqg1PkIBOSiwkIMTkzCWOtgNRUk")
+                bot.send_message(
+                    chat_id=chatid, text=f"{notice[i]} {url['message']} {link}"
+                )
+                root.info("chat")
+            else:
+                root.info("nothing change")
+            root.info(msg=f"waitng for { int(sleep_sec)/len(urls) } seconds")
+            time.sleep(int(sleep_sec) / len(urls))
 finally:
     driver.close()
